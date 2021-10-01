1b6d8584a157eaf2492cedb73e8512024396b26d
szyy666 <65032274+szyy666@users.noreply.github.com>
2021-10-01 19:44:03 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,42 +2,42 @@
 export const VITE_APP_CONFIG = {
 
   // 站点名
-  SiteName: 'Hi, I\'m mjj',
+  SiteName: 'SZYY\的\个人介绍',
   
   // 首页logo名
-  logoName: 'MJJ',
+  logoName: 'SZYY',
 
   // 第一行描述
   line1Desc: 'I don\'t have to, but I have to',
 
   // 第二行描述
-  line2Desc: 'Sorry, I\'m mjj ：）',
+  line2Desc: 'Sorry, I\'m SZYY ：）',
 
   // 项目描述
   projectDesc : [
     {
       id: 1,
-      subtitle: 'haha',
-      title: 'This project is for mjj',
+      subtitle: 'hi',
+      title: 'I\'m\a\student\in\HF',
       img: 'img1',
     },
     {
       id: 2,
-      subtitle: 'hoho',
-      title: 'something wonderful is about to happen',
+      subtitle: 'About\me',
+      title: 'I\love\playing\computer\games',
       img: 'img2',
     },
     {
       id: 3,
-      subtitle: 'hehe',
-      title: 'Best wishes to mjj',
+      subtitle: 'And',
+      title: 'Best wishes to you',
       img: 'img3',
     },
   ],
 
   // Email
-  email: 'mjj@mjj.com',
+  email: 'shenzhouyangyang@gmail.com',
 
   // phone
-  phone: '188-8888-8888'
+  phone: '13927978322'
 };
